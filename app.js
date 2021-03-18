ae5639459aacd013bb92c942b0be197c6f2927a2
dana-younis <younisdana7@gmail.com>
2021-03-18 15:43:30 +0200

    lab4

---
+++ app.js
@@ -0,0 +1,23 @@
+//data type
+// hello in the world of cars
+//my name is dana younis and this is 2021
+
+//var yourCarsYear="2021";
+
+var yourFavoriteTypeOfCars="welcome To The World Of Cars";
+// console.log('hi------------------>')
+// console.log(yourFavoriteTypeOfCars)
+alert(yourFavoriteTypeOfCars)
+var theConditionOfYourCar =" YOU must Have A lot Of Money To Buy One Of These Cars";
+alert(theConditionOfYourCar)
+var yourFavoriteTypeOfCar= prompt("what is yourFavorite Type Of Car")
+confirm("2021");
+
+
+var yourCarType= prompt("what is your type car?");
+
+if (yourCarType=='Rolls Royce') {document.write('<img src="https://media.foxbusiness.com/BrightCove/854081161001/201701/3996/854081161001_5279496769001_A-new-Dawn-for-Rolls-Royce.jpg?pubId=854081161001"/>');}
+else if(yourCarType=='Pagani Zonda'){document.write('<img src="https://th.bing.com/th/id/OIP.PsvkytL-6TVt1X6HarUp_wHaE8?pid=ImgDet&rs=1"/>');}
+    
+
+else{alert('choose between Rolls Royce orPagani Zonda');}
