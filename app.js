869023d69c7f107c2f83a017a45a58d92952da0a
dana-younis <younisdana7@gmail.com>
2021-03-21 14:08:44 +0200

    lab5

--- app.js
+++ app.js
@@ -19,10 +19,21 @@ var yourCarType= prompt("what is your car type?");
  alert( "hello" + yourCarType);
 
 var yourFavoriteCarType= prompt("what is yourFavorite Type Of Car?");
+while(yourFavoriteCarType !=='ferrari' && yourFavoriteCarType!=='bmw')
+{yourFavoriteCarType=prompt('please only write bmw/ferrari');}
 
+var numberOfPictures = prompt('how many pictures do you want?');
 
-if (yourFavoriteCarType=='ferrari'){document.write('<img src="https://th.bing.com/th/id/OIP.KCbdnzT2uU13heR5RwejNgHaE8?pid=ImgDet&rs=1" />');}
-else if(yourFavoriteCarType=='bmw'){document.write('<img src="https://th.bing.com/th/id/OIP.nvcFt1-KD7y-m9pKAxeftgHaE7?w=242&h=180&c=7&o=5&pid=1.7" />') ;}
+var pictures ='';
+var buy='';
+
+if (yourFavoriteCarType=='ferrari'){pictures=('<img src="https://th.bing.com/th/id/OIP.KCbdnzT2uU13heR5RwejNgHaE8?pid=ImgDet&rs=1" />') ;}
+
+else if(yourFavoriteCarType=='bmw'){pictures=('<img src="https://th.bing.com/th/id/OIP.nvcFt1-KD7y-m9pKAxeftgHaE7?w=242&h=180&c=7&o=5&pid=1.7" />') ;}
     
 
-else{alert('choose between ferrari  or bmw');}
+//else{alert('choose between ferrari  or bmw');}
+ console.log(pictures);
+ for (var j=0;j < numberOfPictures;j++){buy += pictures;}
+ console.log(buy);
+ document.write(buy)
